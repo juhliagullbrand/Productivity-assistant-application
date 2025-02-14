4d614ca50f28bf56906d4be67954a4dd322d3e59
Tarane <tarane.eleftheriadis@yh.nackademin.se>
2025-02-14 14:08:34 +0100

    added remove function and all inputs on first row

--- todo.js
+++ todo.js
@@ -1,32 +1,72 @@
-const inputTitle = document.querySelector("#input-todo-title");
-const inputDescription = document.querySelector("#input-todo-description");
+const inputTitle = document.querySelector("#input-title-todo");
+const inputDescription = document.querySelector("#input-description-todo");
 const categoryDropdown = document.querySelector("#category-todo");
 const inputDeadline = document.querySelector("#deadline-todo");
-const inputTimeEstimate = document.querySelector("#time-estimate");
+const inputTimeEstimate = document.querySelector("#time-estimate-todo");
 const btn = document.querySelector(".btn-todo");
-const container = document.querySelector(".flex-container-todo");
+const container = document.querySelector(".box-todo");
+
+let resultTextDiv;
+let resultIconDiv;
+let resultContainer;
+let selectResultContainer;
 
 btn.addEventListener("click", () => {
-    let inputContainer = document.createElement("div");
-    inputContainer.classList.add("input-container-result-todo");
-    let result = document.createElement("p");
-    result.innerHTML = `<strong>${inputTitle.value}</strong><br>${inputDescription.value}`;
+  resultContainer = document.createElement("div");
+  resultContainer.classList.add("resultContainer-todo");
 
-    createEditDelete(inputContainer);
+  resultTextDiv = document.createElement("div");
+  resultTextDiv.classList.add("resultTextDiv-todo");
+  resultTextDiv.innerHTML = `<strong>${inputTitle.value}</strong><br>${inputDescription.value}`;
 
-    container.append(inputContainer);
-    inputContainer.append(result);
-})
+  selectResultContainer = document.createElement("div");
+  selectResultContainer.classList.add("selectResultContainer-todo");
+  selectResultContainer.innerHTML = `${categoryDropdown.value} ${inputDeadline.value} ${inputTimeEstimate.value}`
 
-const createEditDelete = (inputContainer) => {
-    let edit = document.createElement("button");
-    edit.style.backgroundImage = `<img src="icon/delete.png">`;
-    
-    inputContainer.append(edit);
+  resultIconDiv = document.createElement("div");
+  resultIconDiv.classList.add("resultIconDiv-todo");
 
-    deleteBtn.addEventListener("click", () => {
-        inputContainer.remove();
-    });
+  resultContainer.append(resultTextDiv, resultIconDiv);
+  container.append(resultContainer, selectResultContainer); 
 
+  inputTitle.value = "";
+  inputDescription.value = "";
+
+  createEditButton();
+  createDeleteButton();
+});
+
+const createEditButton = () => {
+  let editBtn = document.createElement("button");
+  editBtn.classList.add("edit-btn-todo");
+
+  let iconEdit = document.createElement("img");
+  iconEdit.src = "/icon/edit.png";
+  iconEdit.style.width = "30px";
+
+  editBtn.append(iconEdit);
+  resultIconDiv.append(editBtn);
+
+  editBtn.addEventListener("click", () => {
+    resultContainer.edit();
+  })
 };
+
+const createDeleteButton = () => {
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn-todo");
+
+  let iconDelete = document.createElement("img");
+  iconDelete.src = "/icon/delete.png";
+  iconDelete.style.width = "30px";
+
+  deleteBtn.append(iconDelete);
+  resultIconDiv.append(deleteBtn);
+
+  deleteBtn.addEventListener("click", () => {
+    resultContainer.remove();
+  });
+};
+
+
 
