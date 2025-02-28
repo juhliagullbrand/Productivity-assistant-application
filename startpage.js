4bf2d24ba2fdda27d34ccd23bb5fe009379d340c
Juhlia Gullbrand <juhlia.gullbrand@hotmail.se>
2025-02-28 14:28:37 +0100

    quote API on startpage done

--- startpage.js
+++ startpage.js
@@ -1,4 +1,6 @@
 const displayQuote = document.querySelector("#displayQuote");
+const pQuote = document.querySelector("#quote");
+const pAuthor = document.querySelector("#author");
 
 const displayTodoUl = document.querySelector("#displayTodoUl");
 const displayHabitsUl = document.querySelector("#displayHabitsUl");
@@ -13,3 +15,22 @@ const latestEvents = events
 .sort((a, b) => new Date(a.start) - new (b.start))
 .slice(0, 3); 
 
+let getData = async () => {
+    const url = "https://dummyjson.com/quotes/random";
+    try {
+        let response = await fetch(url);
+        let json = await response.json();
+        return json
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+let renderPage = async () => {
+    let quotes = await getData();
+    pQuote.innerText = quotes.quote;
+    pAuthor.innerText = quotes.author;
+}
+
+renderPage();
